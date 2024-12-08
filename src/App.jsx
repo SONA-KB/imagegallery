ca1cb88af0c51a29b95725761b370acf2fc5a9d6
sona kb <sonakb16@gmail.com>
2024-12-08 19:17:15 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import Gallery from './components/Gallery'
+
+function App() {
+  
+
+  return (
+    
+     <>
+     <div>
+      <Gallery/>
+     </div>
+     </>
+    
+  )
+}
+
+export default App
